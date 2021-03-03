2d5a0b15879b5a0323f7cf7709fb62edca92287f
Changpeng Duan <838560574@qq.com>
2021-03-03 14:46:29 +0800

    test sass func

--- src/components/timePart.vue
+++ src/components/timePart.vue
@@ -1,301 +1,316 @@
 <template>
-  <div class="timePart">
-    <div class="lt">
-      <h5>活动限界 <s>ACTIVE RANGE</s></h5>
-      <P>{{ time }}</P>
-      <span>{{ percent }}%</span>
-      <ul>
-        <li>
-          <em>STOP</em>
-          <i v-if="i == 1"></i>
-        </li>
-        <li>
-          <em>SLOW</em>
-          <i v-if="i == 2"></i>
-        </li>
-        <li>
-          <em>NORMAL</em>
-          <i v-if="i == 3"></i>
-        </li>
-        <li>
-          <em>RACING</em>
-          <i v-if="i == 4"></i>
-        </li>
-      </ul>
-    </div>
-    <div class="rt">
-      <div class="top">
-        <h5>内部</h5>
-        <s>INTERNAL</s>
-        <i v-if="type"></i>
-      </div>
-      <div class="middle">
-        <span>主电源供给</span>
-        <s>MAIN POWER SUPPLE</s>
-      </div>
-      <div class="bottom">
-        <h5>外部</h5>
-        <s>EXTERNAL</s>
-        <i v-if="!type"></i>
-      </div>
-    </div>
-  </div>
+	<div class="timePart">
+		<div class="lt">
+			<h5>活动限界 <s>ACTIVE RANGE</s></h5>
+			<P>{{ time }}</P>
+			<span>{{ percent }}%</span>
+			<ul>
+				<li>
+					<em>STOP</em>
+					<i v-if="i == 1"></i>
+				</li>
+				<li>
+					<em>SLOW</em>
+					<i v-if="i == 2"></i>
+				</li>
+				<li>
+					<em>NORMAL</em>
+					<i v-if="i == 3"></i>
+				</li>
+				<li>
+					<em>RACING</em>
+					<i v-if="i == 4"></i>
+				</li>
+			</ul>
+		</div>
+		<div class="rt">
+			<div class="top">
+				<h5>内部</h5>
+				<s>INTERNAL</s>
+				<i v-if="type"></i>
+			</div>
+			<div class="middle">
+				<span>主电源供给</span>
+				<s>MAIN POWER SUPPLE</s>
+			</div>
+			<div class="bottom">
+				<h5>外部</h5>
+				<s>EXTERNAL</s>
+				<i v-if="!type"></i>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import global from '../Global'
+import global from "../Global";
 
 export default {
-  data() {
-    return {
-      i: 1,
-      type: true,
-      time: '8:88:88',
-      percent: '00',
-    }
-  },
-  mounted() {
-    this.mockDate();
-    this.timer = setInterval(() => {
-      this.mockDate();
-    }, 1000);
-  },
-  beforeDestroy() {
-    clearInterval(this.timer);
-  },
-  methods: {
-    mockDate() {
-      this.time = globalcurrentTime();
-      this.percent = (Math.random(0, 100) * 100).toFixed(0);
-      this.type = !this.type;
-      this.i = parseInt(Math.random(1, 4) * 10);
-    }
-  }
-}
+	data() {
+		return {
+			i: 1,
+			type: true,
+			time: "8:88:88",
+			percent: "00",
+		};
+	},
+	mounted() {
+		this.mockDate();
+		this.timer = setInterval(() => {
+			this.mockDate();
+		}, 1000);
+	},
+	beforeDestroy() {
+		clearInterval(this.timer);
+	},
+	methods: {
+		mockDate() {
+			this.time = globalcurrentTime();
+			this.percent = (Math.random(0, 100) * 100).toFixed(0);
+			this.type = !this.type;
+			this.i = parseInt(Math.random(1, 4) * 10);
+		},
+	},
+};
 </script>
 
+
 <style scoped>
-@font-face {
+/* @font-face {
   font-family: UnidreamLED;
   src: url('../assets/font/UnidreamLED.ttf');
+} */
+
+ul,
+li {
+	list-style: none;
+	margin: 0;
+	padding: 0;
 }
 
-ul, li {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-em, s, i {
-  font-style: normal;
-  text-decoration: none;
+em,
+s,
+i {
+	font-style: normal;
+	text-decoration: none;
 }
 
 .timePart {
-  width: 400px;
-  height: 200px;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-
+	width: 400px;
+	height: 200px;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
 }
 
 .timePart .lt {
-  width: 300px;
-  overflow: hidden;
-  float: left;
+	width: 300px;
+	overflow: hidden;
+	float: left;
 }
 
 .timePart .rt {
-  width: 100px;
-  overflow: hidden;
-  float: right;
+	width: 100px;
+	overflow: hidden;
+	float: right;
 }
 
 .timePart .lt h5 {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  font-size: 14px;
-  font-weight: normal;
-  color: #01355B;
-  text-align: left;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	font-size: 14px;
+	font-weight: normal;
+	color: #01355b;
+	text-align: left;
 }
 
 .timePart .lt s {
-  font-size: 14px;
+	font-size: 14px;
 }
 
 .timePart .lt p {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  font-size: 60px;
-  color: #01355B;
-  text-align: left;
-  font-family: UniDreamLED;
-  letter-spacing: 3px;
-  margin-top: 3px;
-  margin-bottom: 3px;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	font-size: 60px;
+	color: #01355b;
+	text-align: left;
+	font-family: UniDreamLED;
+	letter-spacing: 3px;
+	margin-top: 3px;
+	margin-bottom: 3px;
 }
 
 .timePart .lt span {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  font-size: 14px;
-  text-align: right;
-  font-family: UniDreamLED;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	font-size: 14px;
+	text-align: right;
+	font-family: UniDreamLED;
 }
 
 .timePart .lt ul {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
 }
 
 .timePart .lt li {
-  width: 23%;
-  height: 40px;
-  background: #01355B;
-  color: #fff;
-  text-align: center;
-  float: left;
-  margin-right:2px;
-  font-size:14px;
-  padding-top:12px;
-  padding-bottom:12px;
+	width: 23%;
+	height: 40px;
+	background: #01355b;
+	color: #fff;
+	text-align: center;
+	float: left;
+	margin-right: 2px;
+	font-size: 14px;
+	padding-top: 12px;
+	padding-bottom: 12px;
 }
 
 .timePart .lt li:nth-child(4) {
-  margin-right: 0;
+	margin-right: 0;
 }
 
 .timePart .lt li i {
-  width: 90%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  margin-top:12px;
-  height:12px;
-  background: #fff;
+	width: 90%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	margin-top: 12px;
+	height: 12px;
+	background: #fff;
 }
 
 .rt .top {
-  width: 100%;
-  height: 50px;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  background: #01355B;
-  color: #fff;
+	width: 100%;
+	height: 50px;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	background: #01355b;
+	color: #fff;
 }
 
 .rt .top h5 {
-  width: 40px;
-  font-size: 20px;
-  margin: 0;
-  float: left;
-  text-align: left;
-  padding-left: 20px;
+	width: 40px;
+	font-size: 20px;
+	margin: 0;
+	float: left;
+	text-align: left;
+	padding-left: 20px;
 }
 
 .rt .top s {
-  float: left;
-  font-size: 0.80px;
-  text-align: left;
-  padding-left: 1.20px;
+	float: left;
+	font-size: 0.8px;
+	text-align: left;
+	padding-left: 1.2px;
 }
 
 .rt .top i {
-  position: relative;
-  top: -20px;
-  right: 10px;
-  float: right;
-  width: 15px;
-  height: 30px;
-  background: linear-gradient(135deg, #fff 25%, #005EA2 0,
-  #005EA2 50%, #fff 0,
-  #fff 75%, #005EA2 0);
-  background-size: 12px 12px;
+	position: relative;
+	top: -20px;
+	right: 10px;
+	float: right;
+	width: 15px;
+	height: 30px;
+	background: linear-gradient(
+		135deg,
+		#fff 25%,
+		#005ea2 0,
+		#005ea2 50%,
+		#fff 0,
+		#fff 75%,
+		#005ea2 0
+	);
+	background-size: 12px 12px;
 }
 
 .rt .middle {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  margin-top:5px;
-  margin-bottom:5px;
-  background: #01355B;
-  color: #fff;
-  padding-top: 3px;
-  padding-bottom: 3px;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	margin-top: 5px;
+	margin-bottom: 5px;
+	background: #01355b;
+	color: #fff;
+	padding-top: 3px;
+	padding-bottom: 3px;
 }
 
 .middle span {
-  width: 100%;
-  margin: 0 auto;
-  display: block;
-  font-size: 14px;
-  text-align: left;
-  padding-left: 10px;
+	width: 100%;
+	margin: 0 auto;
+	display: block;
+	font-size: 14px;
+	text-align: left;
+	padding-left: 10px;
 }
 
 .middle s {
-  width: 100%;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  font-size: 12px;
+	width: 100%;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	font-size: 12px;
 }
 
 .rt .bottom {
-  width: 100%;
-  height: 50px;
-  overflow: hidden;
-  display: block;
-  margin: 0 auto;
-  background: #01355B;
-  color: #fff;
-  padding-top: 3px;
-  padding-bottom: 3px;
+	width: 100%;
+	height: 50px;
+	overflow: hidden;
+	display: block;
+	margin: 0 auto;
+	background: #01355b;
+	color: #fff;
+	padding-top: 3px;
+	padding-bottom: 3px;
 }
 
 .rt .bottom h5 {
-  width: 80px;
-  font-size: 24px;
-  margin: 0;
-  float: left;
-  text-align: left;
-  padding-left: 20px;
+	width: 80px;
+	font-size: 24px;
+	margin: 0;
+	float: left;
+	text-align: left;
+	padding-left: 20px;
 }
 
 .rt .bottom s {
-  float: left;
-  font-size: 12px;
-  text-align: left;
-  padding-left: 20px;
+	float: left;
+	font-size: 12px;
+	text-align: left;
+	padding-left: 20px;
 }
 
 .rt .bottom s {
-  font-size: 12px;
+	font-size: 12px;
 }
 
 .rt .bottom i {
-  position: relative;
-  top: -20px;
-  right: 10px;
-  float: right;
-  width: 15px;
-  height: 30px;
-  background: linear-gradient(135deg, #fff 25%, #005EA2 0,
-  #005EA2 50%, #fff 0,
-  #fff 75%, #005EA2 0);
-  background-size: 12px 12px;
+	position: relative;
+	top: -20px;
+	right: 10px;
+	float: right;
+	width: 15px;
+	height: 30px;
+	background: linear-gradient(
+		135deg,
+		#fff 25%,
+		#005ea2 0,
+		#005ea2 50%,
+		#fff 0,
+		#fff 75%,
+		#005ea2 0
+	);
+	background-size: 12px 12px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
-module.exports = {  
-    lintOnSave: false,
-    devServer: {
-        port: 8214, // 端口
-        overlay: {
-            warnings: false,
-            errors: false
-        },
-        proxy: {
-            '/api': {
-                target: 'http://192.168.0.162:19080/',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': '',
-                }
-            }
-        }
-    },
-    // 输出文件目录
-    outputDir: 'D:/wwwroot/test/new_back/static_pc',
+module.exports = {
+	lintOnSave: false,
+	devServer: {
+		port: 8214, // 端口
+		overlay: {
+			warnings: false,
+			errors: false,
+		},
+		proxy: {
+			"/api": {
+				target: "http://192.168.0.162:19080/",
+				changeOrigin: true,
+				pathRewrite: {
+					"^/api": "",
+				},
+			},
+		},
+	},
+	// 输出文件目录
+	outputDir: "D:/wwwroot/test/new_back/static_pc",
 };
